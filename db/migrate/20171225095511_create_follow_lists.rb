b9c6d662b4e5f2270a113c818349e00015c99e94
kaiseiHata <hat10w01u@gmail.com>
2017-12-25 19:18:01 +0900

    user_id,screen_nameカラムを追加

--- db/migrate/20171225095511_create_follow_lists.rb
+++ db/migrate/20171225095511_create_follow_lists.rb
@@ -1,7 +1,8 @@
 class CreateFollowLists < ActiveRecord::Migration[5.1]
   def change
     create_table :follow_lists do |t|
-
+      t.string :screen_name
+      t.string :user_id
       t.timestamps
     end
   end
